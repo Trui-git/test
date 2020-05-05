2e77109b727f2afc9432fb5e3318a164bf254fea
tengrui <tengrui13@gmail.com>
2020-05-05 08:03:58 -0400

    Updates

---
+++ precache-manifest.7e4a0aa40abe3adaac3d2e59905be687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16084fd532533d4668df8ff48aa5300",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "d4c32babc121f33f73d2",
+    "url": "/test/static/css/2.195e2498.chunk.css"
+  },
+  {
+    "revision": "a1a7e9b30b054c6f67c6",
+    "url": "/test/static/css/main.e6a37a0a.chunk.css"
+  },
+  {
+    "revision": "d4c32babc121f33f73d2",
+    "url": "/test/static/js/2.f99fbe76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test/static/js/2.f99fbe76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a7e9b30b054c6f67c6",
+    "url": "/test/static/js/main.9cc8f5dd.chunk.js"
+  },
+  {
+    "revision": "c693572e290071aff2fe",
+    "url": "/test/static/js/runtime-main.80b9e57a.js"
+  }
+]);
